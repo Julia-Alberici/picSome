bee11ac0f81d0ea6e2af30c104cb4ed9b75e545c
Julia Alberici <julia.jpa0@gmail.com>
2022-09-16 21:50:28 -0300

    fetching photos and pass it to context

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,10 +1,20 @@
-import {createContext} from 'react';
+import {createContext, useEffect, useState} from 'react';
 
 const Context = createContext();
 
 function ContextProvider({children}){
+    const [photos, setPhotos] = useState([])
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json";
+
+    useEffect(() =>{
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setPhotos(data));
+    }, [])
+
+    console.log(photos)
     return(
-        <Context.Provider value="">
+        <Context.Provider value={{photos}}>
             {children}
         </Context.Provider>
     )
