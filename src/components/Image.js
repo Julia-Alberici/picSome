fa1ed500dd31b888c7c210de89b382736a920c5e
Julia Alberici <julia.jpa0@gmail.com>
2022-09-16 22:40:05 -0300

    add hover state

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 function Image({className, image}){
+    const [hovered, setHovered] = useState(false);
+    console.log(hovered)
     return(
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`}
+            onMouseOver={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <img src={image.url} className="image-grid"/>
         </div>
     )
