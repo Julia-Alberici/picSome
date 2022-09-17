b053b8af42425f7a6bf55482852512bd25da1b8a
Julia Alberici <julia.jpa0@gmail.com>
2022-09-16 22:18:51 -0300

    mapping photos and displaying it

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,8 +1,18 @@
+import { useContext } from 'react';
+import { Context } from "../AppContext";
+import Image from "../components/Image"
+import { getClass } from "../utils"
+
 function Photos() {
+    const {photos} = useContext(Context);
+
+    const imageEl = photos.map((image, index) => (
+        <Image key={image.id} image={image} className={getClass(index)}/>
+    ))
     return (
         <>
             <main className="photos">
-                <h1>Images go here</h1>
+                {imageEl}
             </main>
         </>
     )
