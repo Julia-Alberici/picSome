cb01f38b93d94fc9e033775c0ac8bff31911d12f
Julia Alberici <julia.jpa0@gmail.com>
2022-09-19 20:52:34 -0300

    change cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
+import { useContext } from 'react';
 import {Link} from 'react-router-dom';
+import { Context } from '../AppContext';
 
 function Header() {
+    const {cartItems} = useContext(Context)
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
-            <Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+            <Link to="/cart">
+                <i className={`ri-shopping-cart-${cartItems.length ? "fill" : "line"} ri-fw ri-2x`}></i>
+            </Link>
         </header>
     )
 }
